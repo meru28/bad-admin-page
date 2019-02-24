b201494226e85a30c261bbba6c1605feb7597c02
merurend <merurw@gmail.com>
2019-02-24 07:46:50 +0700

    modal add sukse tp msh krg rapi

--- src/components/ModalAddProduct.jsx
+++ src/components/ModalAddProduct.jsx
@@ -81,6 +81,9 @@ ${deskripsi}`);
         alert(`Sukses Tambah Product ${this.state.judul}`);
         this.toggle();
       })
+      .then(() => {
+        window.location.reload(true);
+      })
       .catch(err => {
         console.log(err);
       });
@@ -104,147 +107,145 @@ ${deskripsi}`);
               <div className="wrapper wrapper--w790">
                 <div className="card card-5">
                   <div className="card-body">
-                    <form method="POST">
-                      <div className="form-row">
-                        <div className="name">Nama Obat</div>
-                        <div className="value">
-                          <div className="row row-space">
-                            <div className="input-group-desc">
-                              <input
-                                className="input--style-5"
-                                type="text"
-                                name="judul"
-                                onChange={this.handleChange}
-                              />
-                            </div>
+                    <div className="form-row">
+                      <div className="name">Nama Obat</div>
+                      <div className="value">
+                        <div className="row row-space">
+                          <div className="input-group-desc">
+                            <input
+                              className="input--style-5"
+                              type="text"
+                              name="judul"
+                              onChange={this.handleChange}
+                            />
                           </div>
                         </div>
                       </div>
-                      <div className="form-row">
-                        <div className="name">Harga</div>
-                        <div className="value">
-                          <div className="row row-space">
-                            <div className="input-group-desc">
-                              <input
-                                className="input--style-5"
-                                type="text"
-                                name="harga"
-                                onChange={this.handleChange}
-                              />
-                            </div>
+                    </div>
+                    <div className="form-row">
+                      <div className="name">Harga</div>
+                      <div className="value">
+                        <div className="row row-space">
+                          <div className="input-group-desc">
+                            <input
+                              className="input--style-5"
+                              type="text"
+                              name="harga"
+                              onChange={this.handleChange}
+                            />
                           </div>
                         </div>
                       </div>
-                      <div className="form-row">
-                        <div className="name">Cara Pakai</div>
-                        <div className="value">
-                          <div className="row row-space">
-                            <div className="input-group-desc">
-                              <input
-                                className="input--style-5"
-                                type="text"
-                                name="caraPakai"
-                                onChange={this.handleChange}
-                              />
-                            </div>
+                    </div>
+                    <div className="form-row">
+                      <div className="name">Cara Pakai</div>
+                      <div className="value">
+                        <div className="row row-space">
+                          <div className="input-group-desc">
+                            <input
+                              className="input--style-5"
+                              type="text"
+                              name="caraPakai"
+                              onChange={this.handleChange}
+                            />
                           </div>
                         </div>
                       </div>
-                      <div className="form-row">
-                        <div className="name">Gambar 1</div>
-                        <div className="value">
-                          <div className="row row-space">
-                            <div className="input-group-desc">
-                              <input
-                                className="input--style-5"
-                                type="text"
-                                name="imageUrl"
-                                onChange={this.handleChange}
-                              />
-                            </div>
+                    </div>
+                    <div className="form-row">
+                      <div className="name">Gambar 1</div>
+                      <div className="value">
+                        <div className="row row-space">
+                          <div className="input-group-desc">
+                            <input
+                              className="input--style-5"
+                              type="text"
+                              name="imageUrl"
+                              onChange={this.handleChange}
+                            />
                           </div>
                         </div>
                       </div>
-                      <div className="form-row">
-                        <div className="name">Gambar 2</div>
-                        <div className="value">
-                          <div className="row row-space">
-                            <div className="input-group-desc">
-                              <input
-                                className="input--style-5"
-                                type="text"
-                                name="imageUrl1"
-                                onChange={this.handleChange}
-                              />
-                            </div>
+                    </div>
+                    <div className="form-row">
+                      <div className="name">Gambar 2</div>
+                      <div className="value">
+                        <div className="row row-space">
+                          <div className="input-group-desc">
+                            <input
+                              className="input--style-5"
+                              type="text"
+                              name="imageUrl1"
+                              onChange={this.handleChange}
+                            />
                           </div>
                         </div>
                       </div>
-                      <div className="form-row">
-                        <div className="name">Penyimpanan</div>
-                        <div className="value">
-                          <div className="row row-space">
-                            <div className="input-group-desc">
-                              <input
-                                className="input--style-5"
-                                type="text"
-                                name="storageInfo"
-                                onChange={this.handleChange}
-                              />
-                            </div>
+                    </div>
+                    <div className="form-row">
+                      <div className="name">Penyimpanan</div>
+                      <div className="value">
+                        <div className="row row-space">
+                          <div className="input-group-desc">
+                            <input
+                              className="input--style-5"
+                              type="text"
+                              name="storageInfo"
+                              onChange={this.handleChange}
+                            />
                           </div>
                         </div>
                       </div>
-                      <div className="form-row">
-                        <div className="name">Stock</div>
-                        <div className="value">
-                          <div className="row row-space">
-                            <div className="input-group-desc">
-                              <input
-                                className="input--style-5"
-                                type="text"
-                                name="stock"
-                                onChange={this.handleChange}
-                              />
-                            </div>
+                    </div>
+                    <div className="form-row">
+                      <div className="name">Stock</div>
+                      <div className="value">
+                        <div className="row row-space">
+                          <div className="input-group-desc">
+                            <input
+                              className="input--style-5"
+                              type="text"
+                              name="stock"
+                              onChange={this.handleChange}
+                            />
                           </div>
                         </div>
                       </div>
-                      <div className="form-row">
-                        <div className="name">Deskripsi</div>
-                        <div className="value">
-                          <div className="row row-space">
-                            <div className="input-group-desc">
-                              <textarea
-                                className="input--style-5"
-                                type="text"
-                                name="deskripsi"
-                                onChange={this.handleChange}
-                              />
-                            </div>
+                    </div>
+                    <div className="form-row">
+                      <div className="name">Deskripsi</div>
+                      <div className="value">
+                        <div className="row row-space">
+                          <div className="input-group-desc">
+                            <textarea
+                              className="input--style-5"
+                              type="text"
+                              name="deskripsi"
+                              onChange={this.handleChange}
+                            />
                           </div>
                         </div>
                       </div>
+                    </div>
 
-                      <div className="form-row">
-                        <div className="name">Subject</div>
-                        <div className="value">
-                          <div className="input-group">
-                            <div className="rs-select2 js-select-simple select--no-search">
-                              <select name="subject">
-                                <option disabled="disabled" selected="selected">
-                                  Choose option
-                                </option>
-                                <option>Subject 1</option>
-                                <option>Subject 2</option>
-                                <option>Subject 3</option>
-                              </select>
-                              <div className="select-dropdown" />
-                            </div>
+                    <div className="form-row">
+                      <div className="name">Subject</div>
+                      <div className="value">
+                        <div className="input-group">
+                          <div className="rs-select2 js-select-simple select--no-search">
+                            <select name="subject">
+                              <option disabled="disabled" selected="selected">
+                                Choose option
+                              </option>
+                              <option>Subject 1</option>
+                              <option>Subject 2</option>
+                              <option>Subject 3</option>
+                            </select>
+                            <div className="select-dropdown" />
                           </div>
                         </div>
                       </div>
-                    </form>
+                    </div>
                   </div>
                 </div>
               </div>
